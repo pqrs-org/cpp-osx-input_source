21ea103d8596c0889c755c609a3a8f880c3fa0d8
Takayama Fumihiko <tekezo@pqrs.org>
2019-01-28 20:37:41 +0900

    version 2.6.0

--- include/pqrs/osx/input_source.hpp
+++ include/pqrs/osx/input_source.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-// pqrs::osx::input_source v2.5
+// pqrs::osx::input_source v2.6
 
 // (C) Copyright Takayama Fumihiko 2019.
 // Distributed under the Boost Software License, Version 1.0.
