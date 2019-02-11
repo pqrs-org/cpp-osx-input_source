4299fbfc759b05946e2eef785124eec680c2d2ca
Takayama Fumihiko <tekezo@pqrs.org>
2019-02-11 18:28:54 +0900

    use pqrs::json::unmarshal_error

--- tests/src/nlohmann_json_test.cpp
+++ tests/src/nlohmann_json_test.cpp
@@ -5,16 +5,6 @@
 
 TEST_CASE("nlohmann_json_properties") {
   {
-    nlohmann::json json;
-    auto properties = json.get<pqrs::osx::input_source::properties>();
-
-    REQUIRE(properties.get_input_source_id() == std::nullopt);
-    REQUIRE(properties.get_localized_name() == std::nullopt);
-    REQUIRE(properties.get_languages().empty());
-    REQUIRE(properties.get_first_language() == std::nullopt);
-  }
-
-  {
     pqrs::osx::input_source::properties properties;
 
     nlohmann::json json = properties;
@@ -35,36 +25,151 @@ TEST_CASE("nlohmann_json_properties") {
     REQUIRE(properties == properties2);
   }
 
-  // Invalid json
+  // from_json
   {
-    nlohmann::json json;
-    json["input_source_id"] = 123;
-    json["localized_name"] = 123;
-    json["languages"] = 123;
-    json["first_language"] = 123;
+    auto json = nlohmann::json::object({
+        {"input_source_id", "com.apple.keylayout.US"},
+        {"localized_name", "U.S."},
+        {"input_mode_id", "com.apple.inputmethod.Japanese"},
+        {"languages", nlohmann::json::array({"en", "af", "asa", "bem"})},
+        {"first_language", "en"},
+    });
+    auto properties = json.get<pqrs::osx::input_source::properties>();
 
+    REQUIRE(properties.get_input_source_id() == "com.apple.keylayout.US");
+    REQUIRE(properties.get_localized_name() == "U.S.");
+    REQUIRE(properties.get_input_mode_id() == "com.apple.inputmethod.Japanese");
+    REQUIRE(properties.get_languages() == std::vector<std::string>{"en", "af", "asa", "bem"});
+    REQUIRE(properties.get_first_language() == "en");
+  }
+
+  // from_json (empty)
+  {
+    auto json = nlohmann::json::object();
     auto properties = json.get<pqrs::osx::input_source::properties>();
 
     REQUIRE(properties.get_input_source_id() == std::nullopt);
     REQUIRE(properties.get_localized_name() == std::nullopt);
+    REQUIRE(properties.get_input_mode_id() == std::nullopt);
     REQUIRE(properties.get_languages().empty());
     REQUIRE(properties.get_first_language() == std::nullopt);
   }
 
-  // Invalid json (languages)
+  // type error
+
   {
-    nlohmann::json json;
-    json["input_source_id"] = 123;
-    json["localized_name"] = 123;
-    json["languages"] = nlohmann::json::array();
-    json["languages"].push_back(123);
-    json["first_language"] = 123;
+    auto json = nlohmann::json();
+    REQUIRE_THROWS_AS(
+        json.get<pqrs::osx::input_source::properties>(),
+        pqrs::json::unmarshal_error);
+    REQUIRE_THROWS_WITH(
+        json.get<pqrs::osx::input_source::properties>(),
+        "json must be object, but is `null`");
+  }
 
-    auto properties = json.get<pqrs::osx::input_source::properties>();
+  // unknown key
 
-    REQUIRE(properties.get_input_source_id() == std::nullopt);
-    REQUIRE(properties.get_localized_name() == std::nullopt);
-    REQUIRE(properties.get_languages().empty());
-    REQUIRE(properties.get_first_language() == std::nullopt);
+  {
+    auto json = nlohmann::json::object({
+        {"input-source-id", "com.apple.keylayout.US"},
+        {"localized_name", "U.S."},
+        {"input_mode_id", "com.apple.inputmethod.Japanese"},
+        {"languages", nlohmann::json::array({"en", "af", "asa", "bem"})},
+        {"first_language", "en"},
+    });
+    REQUIRE_THROWS_AS(
+        json.get<pqrs::osx::input_source::properties>(),
+        pqrs::json::unmarshal_error);
+    REQUIRE_THROWS_WITH(
+        json.get<pqrs::osx::input_source::properties>(),
+        "unknown key: `input-source-id`");
+  }
+
+  // type error (input_source_id)
+
+  {
+    auto json = nlohmann::json::object({
+        {"input_source_id", nlohmann::json()},
+    });
+    REQUIRE_THROWS_AS(
+        json.get<pqrs::osx::input_source::properties>(),
+        pqrs::json::unmarshal_error);
+    REQUIRE_THROWS_WITH(
+        json.get<pqrs::osx::input_source::properties>(),
+        "`input_source_id` must be string, but is `null`");
+  }
+
+  // type error (localized_name)
+
+  {
+    auto json = nlohmann::json::object({
+        {"localized_name", nlohmann::json()},
+    });
+    REQUIRE_THROWS_AS(
+        json.get<pqrs::osx::input_source::properties>(),
+        pqrs::json::unmarshal_error);
+    REQUIRE_THROWS_WITH(
+        json.get<pqrs::osx::input_source::properties>(),
+        "`localized_name` must be string, but is `null`");
+  }
+
+  // type_error (input_mode_id)
+
+  {
+    auto json = nlohmann::json::object({
+        {"input_mode_id", nlohmann::json()},
+    });
+    REQUIRE_THROWS_AS(
+        json.get<pqrs::osx::input_source::properties>(),
+        pqrs::json::unmarshal_error);
+    REQUIRE_THROWS_WITH(
+        json.get<pqrs::osx::input_source::properties>(),
+        "`input_mode_id` must be string, but is `null`");
+  }
+
+  // type_error (languages)
+
+  {
+    auto json = nlohmann::json::object({
+        {"languages", nlohmann::json()},
+    });
+    REQUIRE_THROWS_AS(
+        json.get<pqrs::osx::input_source::properties>(),
+        pqrs::json::unmarshal_error);
+    REQUIRE_THROWS_WITH(
+        json.get<pqrs::osx::input_source::properties>(),
+        "`languages` must be array, but is `null`");
+  }
+
+  // type_error (languages item)
+
+  {
+    auto json = nlohmann::json::object({
+        {"languages", nlohmann::json::array({
+                          "en",
+                          "af",
+                          nlohmann::json(),
+                      })},
+    });
+    REQUIRE_THROWS_AS(
+        json.get<pqrs::osx::input_source::properties>(),
+        pqrs::json::unmarshal_error);
+    REQUIRE_THROWS_WITH(
+        json.get<pqrs::osx::input_source::properties>(),
+        "`languages` must be array of strings, but is `[\"en\",\"af\",null]`");
+  }
+
+  // type_error (first_language)
+
+  {
+    auto json = nlohmann::json::object({
+        {"first_language", nlohmann::json()},
+    });
+    REQUIRE_THROWS_AS(
+        json.get<pqrs::osx::input_source::properties>(),
+        pqrs::json::unmarshal_error);
+    REQUIRE_THROWS_WITH(
+        json.get<pqrs::osx::input_source::properties>(),
+        "`first_language` must be string, but is `null`");
   }
 }
