2a45ffa037887994e59718c77bbf8046fd5f7ea5
Takayama Fumihiko <tekezo@pqrs.org>
2020-04-21 23:24:38 +0900

    update for pqrs::hash changes

--- include/pqrs/osx/input_source/properties.hpp
+++ include/pqrs/osx/input_source/properties.hpp
@@ -97,7 +97,7 @@ struct hash<pqrs::osx::input_source::properties> final {
     size_t h = 0;
 
     if (auto& input_source_id = value.get_input_source_id()) {
-      pqrs::hash_combine(h, *input_source_id);
+      pqrs::hash::combine(h, *input_source_id);
     }
 
     return h;
